48095628c4ce1d86767e84952ae0e0c82cad6219
Park Na Yeon <within197@naver.com>
2022-02-10 13:57:24 +0900

    create a server

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# wetube-clone-v2
+# Wetube-clone-v2
+
 nodeJS를 사용한 유튜브 클론
+
+#### Router
+
+1. Global router  
+   / -> Home  
+   /join -> Join  
+   /login -> Login  
+   /search -> Search
+
+2. User router
+   /users/edit -> Edit user  
+   /users/delete -> Delete user
+
+3. Video router
+   /videos/watch -> Watch Video  
+   /videos/edit -> Edit Video  
+   /videos/delete -> Delete Video  
+   /videos/comments -> Comment on a video  
+   /videos/comments/delete -> Delete A Comment of Video
--- src/server.js
+++ src/server.js
@@ -1,22 +1,47 @@
 import express from "express";
-import morgan from "morgan";
+// import morgan from "morgan";
 
 const PORT = 4000;
 
-const app = express();
-const loggerMiddleware = morgan("dev");
+const app = express(); // Create an Express application
 
-app.use(loggerMiddleware);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-app.get("/", (req, res) => {
-  console.log("I will respond.");
-  return res.send("Hello");
-});
-app.get("/login", (req, res) => {
-  return res.send("Login there");
-});
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed<h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("Home");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login" });
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
-  console.log(`server Listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ server Listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening); // listen fob r connection
+
+// get - http method 'hey get me your login page'
+// http method how to work
+// server is accepting request
+// what is middleware?
