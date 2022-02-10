c8ff2e79d04bb92f79da24ed4fbed0a5085d3e92
Park Na Yeon <within197@naver.com>
2022-02-10 14:02:28 +0900

    add morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,40 +1,22 @@
 import express from "express";
-// import morgan from "morgan";
+import morgan from "morgan"; // middleware
 
 const PORT = 4000;
 
 const app = express(); // Create an Express application
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed<h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.send("Home");
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login" });
+const login = (req, res) => {
+  return res.send(login);
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ server Listening on port http://localhost:${PORT} 🚀`);
@@ -45,3 +27,5 @@ app.listen(PORT, handleListening); // listen fob r connection
 // http method how to work
 // server is accepting request
 // what is middleware?
+
+// middleware => morgan,
