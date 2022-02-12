4174e42274c7f6c005ac529f7df7c333f5276509
Park Na Yeon <within197@naver.com>
2022-02-12 20:50:29 +0900

    Add express session middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan"; // middleware
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,6 +14,28 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!", // 암호화
+    resave: true, // 세션을 항상 저장할지 여부를 정하는 값(false 권장)
+    saveUninitialized: true, // 초기화되지 않은 채 스토어에 저장되는 세션
+  })
+); // 사이트에 들어오는 모두를 기억함
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  // 서버에서 세션에 접근하려면 req.session을 이용한다.
+  return res.send(`${req.session.id}`); //
+  // 브라우저가 서버에게 로그인을 요청할 때, 로그인이 성공한다면 서버는 브라우저에게 session.id(고유)를 줍니다. => cookie
+});
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
