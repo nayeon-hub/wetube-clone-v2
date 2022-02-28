8766257829d7910f5cc0928b4d16c3a440e7107c
Park Na Yeon <within197@naver.com>
2022-02-28 13:05:34 +0900

    Add middleware in videoRouter

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,7 +4,6 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn); // view가 loggedIn, sitename, loggedInUser에 접근하기 위해
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
-  // console.log(res.locals);
   next();
 };
 
