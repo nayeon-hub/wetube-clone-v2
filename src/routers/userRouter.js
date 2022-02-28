1d8b5f2c30aaae46169c311d0113d894816b7e75
Park Na Yeon <within197@naver.com>
2022-02-28 12:19:43 +0900

    Add edit profile controller

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,19 +2,21 @@ import express from "express";
 import {
   see,
   logout,
-  edit,
+  getEdit,
+  postEdit,
   remove,
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin); // github.com에서 설정
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin); // github.com에서 설정
 userRouter.get("/:id", see);
 
 export default userRouter;
